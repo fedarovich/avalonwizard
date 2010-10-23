9abf9559c2e64c278476f73d451c1daf436a5d81
PahanMenski <p.fedarovich@gmail.com>
2010-10-23 21:55:46 +0300

    Initial commit.

---
+++ Source/AvalonWizard/WizardPageInitEventArgs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace AvalonWizard
+{
+    /// <summary>
+    /// Arguments supplied to the <see cref = "WizardPage.Initialize" /> event.
+    /// </summary>
+    public class WizardPageInitEventArgs : RoutedEventArgs
+    {
+        internal WizardPageInitEventArgs(WizardPage page, WizardPage prevPage)
+        {
+            Page = page;
+            PreviousPage = prevPage;
+        }
+
+        /// <summary>
+        /// Gets the <see cref = "WizardPage" /> that has raised the event.
+        /// </summary>
+        /// <value>The wizard page.</value>
+        public WizardPage Page { get; private set; }
+
+        /// <summary>
+        ///   Gets the <see cref = "WizardPage" /> that was previously selected when the event was raised.
+        /// </summary>
+        /// <value>The previous wizard page.</value>
+        public WizardPage PreviousPage { get; private set; }
+    }
+}
