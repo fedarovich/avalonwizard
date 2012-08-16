ba85355458e561b92c05e5c6034fb597e2f0b750
Pavel <Pavel@PS-PC>
2012-08-17 00:07:10 +0300

    Added preliminary version of AvalonWizard MVVM extensions.

---
+++ Source/AvalonWizardSample.Mvvm/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿#region Licence
+// This file is part of AvalonWizard.
+// 
+// AvalonWizard is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// AvalonWizard is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with AvalonWizard.  If not, see <http://www.gnu.org/licenses/>.
+#endregion
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AvalonWizardSample.Mvvm")]
+[assembly: AssemblyDescription("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+    //(used if a resource is not found in the page, 
+    // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+    //(used if a resource is not found in the page, 
+    // app, or any theme specific resource dictionaries)
+)]
+
---
+++ Source/AvalonWizardSample.Mvvm/Views/Page0View.xaml.cs
@@ -0,0 +1,43 @@
+﻿#region Licence
+// This file is part of AvalonWizard.
+// 
+// AvalonWizard is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// AvalonWizard is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with AvalonWizard.  If not, see <http://www.gnu.org/licenses/>.
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AvalonWizardSample.Mvvm.Views
+{
+    /// <summary>
+    /// Interaction logic for Page0View.xaml
+    /// </summary>
+    public partial class Page0View : UserControl
+    {
+        public Page0View()
+        {
+            InitializeComponent();
+        }
+    }
+}
